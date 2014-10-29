6b4e5f700047506e2eec681ec020b9e07e89de9d
Guillaume Van Der Putte <guillaume.tcm@gmail.com>
2014-10-29 18:06:39 +0100

    getting the splash router

--- src/moufla.php
+++ src/moufla.php
@@ -2,7 +2,16 @@
 
 namespace Mouf\Integration\Joomla\Moufla;
 
+use Mouf\Mvc\Splash;
+
 class Moufla {
+
+    function __construct() {
+        var_dump("constructor Moufla");
+        $router = \Mouf::getSplashDefaultRouter();
+        $router->handle();
+    }
+
     public function doSomething() {
         var_dump("level complete"); exit;
     }
