5817bff49ee1a6869cf1b62dc244d03640813b65
Guillaume Van Der Putte <guillaume.tcm@gmail.com>
2014-11-05 11:58:48 +0100

    changing installation on moufla, removing the exceptionRouter

--- src/MouflaInstaller.php
+++ src/MouflaInstaller.php
@@ -24,23 +24,14 @@ class MouflaInstaller implements PackageInstallerInterface {
             $configManager->registerConstant('SECRET', 'string', 'BSHVXjnWTgc5ojRHVyCB', 'A random string. It should be different for any application deployed.');
         }
 
-        // These instances are expected to exist when the installer is run.
-        $httpErrorsController = $moufManager->getInstanceDescriptor('httpErrorsController');
 
         // Let's create the instances.
-        $exceptionRouter = InstallUtils::getOrCreateInstance('exceptionRouter', 'Mouf\\Mvc\\Splash\\Routers\\ExceptionRouter', $moufManager);
         $splashDefaultRouter = InstallUtils::getOrCreateInstance('splashDefaultRouter', 'Mouf\\Mvc\\Splash\\Routers\\SplashDefaultRouter', $moufManager);
         $mouflaNotFoundRouter = InstallUtils::getOrCreateInstance('mouflaNotFoundRouter', 'Mouf\\Integration\\Joomla\\Moufla\\MouflaNotFoundRouter', $moufManager);
         $splashCacheApc = InstallUtils::getOrCreateInstance('splashCacheApc', 'Mouf\\Utils\\Cache\\ApcCache', $moufManager);
         $splashCacheFile = InstallUtils::getOrCreateInstance('splashCacheFile', 'Mouf\\Utils\\Cache\\FileCache', $moufManager);
 
         // Let's bind instances together.
-        if (!$exceptionRouter->getConstructorArgumentProperty('router')->isValueSet()) {
-            $exceptionRouter->getConstructorArgumentProperty('router')->setValue($splashDefaultRouter);
-        }
-        if (!$exceptionRouter->getConstructorArgumentProperty('errorController')->isValueSet()) {
-            $exceptionRouter->getConstructorArgumentProperty('errorController')->setValue($httpErrorsController);
-        }
         if (!$splashDefaultRouter->getConstructorArgumentProperty('fallBackRouter')->isValueSet()) {
             $splashDefaultRouter->getConstructorArgumentProperty('fallBackRouter')->setValue($mouflaNotFoundRouter);
         }
